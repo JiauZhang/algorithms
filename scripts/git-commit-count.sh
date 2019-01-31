38d4afb753880e0871043b11888663f7eb4c03fb
JiauZhang <jiauzhang@gmail.com>
2019-01-31 19:21:48 +0800

    2019-01-31-19:21

---
+++ scripts/git-commit-count.sh
@@ -0,0 +1,1 @@
+git log | grep -e 'commit [a-zA-Z0-9]*' | wc -l
--- src/ugly_number.cpp
+++ src/ugly_number.cpp
@@ -10,12 +10,45 @@ bool isUglyNumber(int num)
 		num /= 3;
 	while (num%5 == 0)
 		num /= 5;
-
+	
+	//cout << "num = " << num << endl;
 	return (num == 1) ? true : false;
+}
+
+void Test(int num)
+{
+	//从输出可以看出，这句代码先执行函数isUglyNumber
+	cout << "number = " << num << "-->" \
+	     << "is ugly: " << isUglyNumber(num) << endl;
+}
+
+int getUglyNumber(int index)
+{
+	if (index <= 0)
+		return -1;
+	int count = 0;
+	int num = 0;
+	while (count < index) {
+		++num;
+		if (isUglyNumber(num))
+			count++;
+	}
+
+	return num;
 }
 
 int main(int argc, char **argv)
 {
-	cout << isUglyNumber(12) << endl;
+	int numbers[5] = {5, 15, 26, 30, 49};
+
+	Test(numbers[0]);
+	Test(numbers[1]);
+	Test(numbers[2]);
+	Test(numbers[3]);
+	Test(numbers[4]);
+
+	int num_idx = getUglyNumber(5);
+	cout << "num_idx: " << num_idx << endl;
+
 	return 0;
 }
